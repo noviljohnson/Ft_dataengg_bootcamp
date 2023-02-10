fd9ddd7e4f99b145669df360d35b64b2d945922b
noviljohnson <noviljohnson9@gmail.com>
2023-02-10 16:40:08 +0530

    new commit

---
+++ labs/shell_scripts/spcl_vars.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#**************************************
+# Shows the usage of special variables
+#**************************************
+
+
+echo "file name is $0"
+echo "first arg is $1"
+
+echo " total number of values passed  :"$#
+echo " all values passed : " $*
+echo "home directory : "$HOME
+
