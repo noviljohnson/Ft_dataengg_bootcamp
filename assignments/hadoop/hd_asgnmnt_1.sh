3e9ab3b4223eb72e2a3fa2fa48c29e77383a5100
Novil Johnson <noviljohnson9@gmail.com>
2023-02-16 17:38:08 +0530

    split and merge hdfs

--- assignments/hadoop/hd_asgnmnt_1.sh
+++ assignments/hadoop/hd_asgnmnt_1.sh
@@ -12,9 +12,17 @@
 
 # hdfs dfs -mkdir /user/training/weather
 hdfs dfs -put /mnt/c/Users/miles.MILE-BL-4766-LA.000/Documents/FT/futurense_hadoop-pyspark/labs/dataset/weather/weather_data.txt  /user/training/weather
-hdfs dfs -cat /user/training/wearther/weather_data.txt
+hdfs dfs -cat /user/training/weather/weather_data.txt
 
 lines=`wc -l /user/training/weather/weather_data.txt`
 
+line=`echo $lines | cut -d' ' -f1`
 
-hdfs dfs -appendtoFile -head  /user/training/weather
+h1=`expr $line / 2`
+h2=`expr $line - $h1`
+
+hdfs dfs -cat /user/training/weather/weather_data.txt | head -$h1 > weather1.txt
+hdfs dfs -cat /user/training/weather/weather_data.txt | tail -$h2 > weather2.txt
+
+cat weather1.txt weather2.txt > weather3.txt
+
