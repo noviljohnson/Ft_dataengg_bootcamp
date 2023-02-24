0167f57796d812a4f6c143e1e30f69a020a3f092
Novil Johnson <noviljohnson9@gmail.com>
2023-02-24 10:27:45 +0530

    weather min max temp spark

--- labs/pyspark/pyspark_app/weather.py
+++ labs/pyspark/pyspark_app/weather.py
@@ -9,13 +9,14 @@ lines = sc.textFile("/mnt/c/Users/miles.MILE-BL-4766-LA.000/Documents/FT/Ft_Bigd
 max_temp = lines.flatMap(lambda x: [x.split()[5]]) \
             .map(lambda x: ('Max_temp',float(x))) \
             .reduceByKey(lambda a, b:max(a,b))
-output = max_temp.collect()
-for (word, count) in output:
-    print(word,count)
+output1 = max_temp.collect()
 
 mim_temp = lines.flatMap(lambda x: [x.split()[6]]) \
             .map(lambda x: ('Min_temp',float(x))) \
-            .reduceByKey(lambda a, b:max(a,b))
+            .reduceByKey(lambda a, b:min(a,b))
 output = mim_temp.collect()
-for (word, count) in output:
-    print(word,count)
+
+print(output1[0][0], output1[0][1])
+print(output[0][0], output[0][1])
+# for (word, count) in output:
+#     print(word,count)
