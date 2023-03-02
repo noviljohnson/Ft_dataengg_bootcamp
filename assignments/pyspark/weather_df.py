f45b2ecd925669541d8b784b9a31ae9144a4cc9a
Novil Johnson <noviljohnson9@gmail.com>
2023-03-02 22:45:45 +0530

    pyspark assgnmnts

--- assignments/pyspark/weather_df.py
+++ assignments/pyspark/weather_df.py
@@ -9,14 +9,38 @@ from pyspark import SparkContext
 sc = SparkContext("local", "weather app")
 
 
-# spark.read('/mnt/c/Users/miles.MILE-BL-4766-LA.000/Documents/FT/Ft_Bigdata/Ft_dataengg_bootcamp/labs/datasets/weather_data.txt')
+# spark.read.load('/mnt/c/Users/miles.MILE-BL-4766-LA.000/Documents/FT/Ft_Bigdata/Ft_dataengg_bootcamp/labs/datasets/weather_data.txt',format = 'csv')
+# df_we.withColumn('_c0',split(regexp_replace(df_we._c0,"\s+",','),','))
 
 lines = sc.textFile("/mnt/c/Users/miles.MILE-BL-4766-LA.000/Documents/FT/Ft_Bigdata/Ft_dataengg_bootcamp/labs/datasets/weather_data.txt")
 
-
 spark = SparkSession.builder.appName("pyspark-examples").getOrCreate()
 
-lines1=lines.flatMap(lambda x: x.split() )#[x.split()[1],x.split()[5],x.split()[6]])
-df = spark.createDataFrame(lines1,schema='_1 string, _2 float, _3 float')
+lines1=lines.map(lambda x: x.split() )
+df = spark.createDataFrame(lines1)
 
-df.withColumn('month',substring(df._2,5,2)).groupBy('month')
+df_r = df.select('_2','_6','_7').withColumn('_6',df._6.cast('float')).withColumn('_7',df._7.cast('float'))
+
+print(df_r.withColumn('month', substring(df_r._2,5,2)).groupBy('month').agg(min(df_r._7),max(df_r._6)).show())
+
+
+# arrayStructureData = [
+#         (("James","","Smith"),["Java","Scala","C++"],"OH","M",{"home":"City Center","work":"City Tech Park"}),
+#         (("Anna","Rose",""),["Spark","Java","C++"],"NY","F",{}),
+#         (("Julia","","Williams"),["CSharp","VB"],"OH","F",{}),
+#         (("Maria","Anne","Jones"),["CSharp","VB"],"NY","M",{}),
+#         (("Jen","Mary","Brown"),["CSharp","VB"],"NY","M",{}),
+#         (("Mike","Mary","Williams"),["Python","VB"],"OH","M",{})
+#         ]
+
+# arrayStructureSchema = StructType([
+#     StructField('name', StructType([
+#             StructField('firstname', StringType(), True),
+#             StructField('middlename', StringType(), True),
+#             StructField('lastname', StringType(), True)
+#             ])),
+#     StructField('languages', ArrayType(StringType()), True),
+#     StructField('state', StringType(), True),
+#     StructField('gender', StringType(), True),
+#     StructField('address', MapType(StringType(),StringType()), True)
+# ])
